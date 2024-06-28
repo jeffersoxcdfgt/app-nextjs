4904a310a46fe2340677744431244dd0138e4adc
Jefferson Medina Cedeno <jefferson.medina@intertecintl.com>
2024-06-27 21:26:21 -0500

    Adding handle errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   try {
@@ -141,6 +142,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
   try {
     const data = await sql<InvoiceForm>`
       SELECT
