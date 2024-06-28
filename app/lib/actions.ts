1bf359b7ab18251d0c8ba9e2973d9311df857c26
Jefferson Medina Cedeno <jefferson.medina@intertecintl.com>
2024-06-27 23:15:47 -0500

    comit new author 3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-// jeffer
+
 
 const FormSchema = z.object({
   id: z.string(),
